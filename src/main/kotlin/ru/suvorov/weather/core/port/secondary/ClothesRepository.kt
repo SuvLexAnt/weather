50d13694ace7a70e29062c55f5338ad2dfa9debb
Lenovo x1 <SuvLexAnt@mail.ru>
2021-02-02 17:28:28 +0300

    New logic. Now providing client all clothes that are for weather not colder that now, so he won't overheat.

--- src/main/kotlin/ru/suvorov/weather/core/port/secondary/ClothesRepository.kt
+++ src/main/kotlin/ru/suvorov/weather/core/port/secondary/ClothesRepository.kt
@@ -4,5 +4,12 @@ import ru.suvorov.weather.core.component.clothes.Clothes
 
 interface ClothesRepository {
 
-    fun getClothesByParams(snow: Boolean, rain: Boolean, temperature: Double, temperatureDiff: Int): List<Clothes>
+    /**
+     * Returns clothes that matches weather conditions
+     * @param snow is there snow outside
+     * @param rain is there rain outside
+     * @param minTemperature minimal acceptable temperature for clothes
+     * @return list of matching clothes
+     */
+    fun getClothesByParams(snow: Boolean, rain: Boolean, minTemperature: Double): List<Clothes>
 }
--- src/main/kotlin/ru/suvorov/weather/core/port/secondary/WeatherFacade.kt
+++ src/main/kotlin/ru/suvorov/weather/core/port/secondary/WeatherFacade.kt
@@ -4,5 +4,9 @@ import ru.suvorov.weather.core.component.weather.Weather
 
 interface WeatherFacade {
 
+    /**
+     * Returns weather conditions by name of geographical place
+     * @param city - name of city or a place
+     */
     fun getWeatherByCity(city: String): Weather
 }
