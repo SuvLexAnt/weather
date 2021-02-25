b18f29be87f00df7bc4e669e0db9792da4c5a1d8
Lenovo x1 <SuvLexAnt@mail.ru>
2021-02-25 19:41:50 +0300

    Added real encoder.

--- src/main/kotlin/ru/suvorov/weather/lib/security/SecurityConfiguration.kt
+++ src/main/kotlin/ru/suvorov/weather/lib/security/SecurityConfiguration.kt
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import javax.sql.DataSource
 
@@ -33,5 +33,5 @@ class SecurityConfiguration(
     }
 
     @Bean
-    fun passwordEncoder(): PasswordEncoder = NoOpPasswordEncoder.getInstance()
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
--- src/main/resources/db/migration/V2__Authorization.sql
+++ src/main/resources/db/migration/V2__Authorization.sql
@@ -3,20 +3,20 @@ create table if not exists users
     id serial8 not null
         constraint users_pk
             primary key,
-    username varchar(50) unique not null,
-    password varchar(50) not null,
+    username varchar(60) unique not null,
+    password varchar(60) not null,
     enabled bool not null
 );
 
 insert into users (username, password, enabled)
 values
-('admin', '1234567Abc', true),
-('user', 'password', true);
+('admin', '$2a$10$ZLhnHxdpHETcxmtEStgpI./Ri1mksgJ9iDP36FmfMdYyVg9g0b2dq', true),
+('user', '$2a$10$ZLhnHxdpHETcxmtEStgpI./Ri1mksgJ9iDP36FmfMdYyVg9g0b2dq', true);
 
 create table if not exists user_roles
 (
-    username varchar(50) not null,
-    role varchar(50) not null
+    username varchar(60) not null,
+    role varchar(60) not null
 );
 
 alter table user_roles
